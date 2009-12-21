2cc4c73f8cbf7988dc06ebf75fa0fbb8278487e9
dcparker <dcparker@gmail.com>
2009-12-21 13:46:16 -0500

    Fixed an issue parsing multipart messages where the boundary was enclosed in quotes.

--- lib/gmail.rb
+++ lib/gmail.rb
@@ -3,7 +3,7 @@ require 'net/smtp'
 require 'smtp_tls'
 
 class Gmail
-  VERSION = '0.0.5'
+  VERSION = '0.0.6'
 
   class NoLabel < RuntimeError; end
 
--- lib/ietf/rfc2045.rb
+++ lib/ietf/rfc2045.rb
@@ -4,7 +4,7 @@ module IETF
     def self.parse_rfc2045_from(raw)
       headers, raw = IETF::RFC822.parse_rfc822_from(raw)
 
-      if headers['content-type'] =~ /multipart\/(\w+); boundary=(.*)/
+      if headers['content-type'] =~ /multipart\/(\w+); boundary=([\"\']?)(.*)\2/
         content = {}
         content[:type] = $1
         content[:boundary] = $2
